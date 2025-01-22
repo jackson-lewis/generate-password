51042e34ec42b63b50557fa980a7b7d083f7ad0e
Jackson Lewis <hello@jacksonlewis.co.uk>
2025-01-22 23:05:47 +0000

    resolved jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,18 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest'
 
 const config: Config = {
   verbose: true,
+  testEnvironment: 'node',
   testPathIgnorePatterns: ['dist/'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!chalk)',
+  ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env'] }]
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
   }
-};
+}
 
 export default config;
